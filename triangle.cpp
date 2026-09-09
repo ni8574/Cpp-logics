afe3e3f88128830ec53ba65c6ffd896f052e5cea
Niharika saini <niharikasaini766@gmail.com>
2026-09-09 13:05:45 +0530

    triangle.changes

---
+++ triangle.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 1;i < n;i++){
+        for(int j = 1;j < i;j++){
+            cout << "  ";
+        
+        
+        for(int k = 2*n-1-2*i;k > 0;k--){
+            cout << " *";
+        }
+        cout << endl;
+    }
+}
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
